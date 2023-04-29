ba1da0d9f7e97cfcfe575e9781550b994fa21e25
Sergey Karinsky <karinsky.dev@gmail.com>
2023-04-29 13:52:39 +0700

    Entry point

--- server/index.js
+++ server/index.js
@@ -24,6 +24,9 @@ app.use(express.static(path.resolve(__dirname, '../client/build')))
 app.use('/api/clan', clanRouter)
 app.use('/api/user', userRouter)
 app.use('/api/news', newsRouter)
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`)
